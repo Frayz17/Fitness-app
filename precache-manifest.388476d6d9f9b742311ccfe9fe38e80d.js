701e7948b5c9aeab50926903540fa6f0efb6b038
Andrey Dmitriev <andrydmit@gmail.com>
2020-01-19 22:08:45 +0200

    Updates

---
+++ precache-manifest.388476d6d9f9b742311ccfe9fe38e80d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3a532580d373896907ef24543cf112d",
+    "url": "/gym-exercises/index.html"
+  },
+  {
+    "revision": "1f73a43c5174bee0bc6b",
+    "url": "/gym-exercises/static/js/2.049c8836.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/gym-exercises/static/js/2.049c8836.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fe3fe207f881f727d78",
+    "url": "/gym-exercises/static/js/main.25215f99.chunk.js"
+  },
+  {
+    "revision": "b35d7175245409904635",
+    "url": "/gym-exercises/static/js/runtime-main.8cb33fd9.js"
+  }
+]);
